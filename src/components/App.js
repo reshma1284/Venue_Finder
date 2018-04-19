238d0964c22197281cd5f8935c4b61be37165c20
devbasket <reshma.dew@gmail.com>
2018-04-19 14:06:55 +0200

    Initializing the Venue list page

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import VenueList from './VenueList';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path='/' component={VenueList} />
+        </Switch>
+      </Router>
+
+    );
+  }
+}
+
+export default App;
---
+++ src/components/VenueList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../styles/VenueList.css';
+
+class VenueList extends Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <h1> Venue Finder App </h1>
+    )
+  }
+}
+
+export default VenueList;
